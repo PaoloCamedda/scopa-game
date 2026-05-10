083791c25004afb957a2faa28640c4c18d27227f
Paolo Camedda <arconet666@gmail.com>
2026-05-10 18:25:38 +0200

    Initial commit: Spring Boot project structure and Model

---
+++ src/main/java/com/example/scopa/ScopaGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.scopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopaGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScopaGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scopa/ScopaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scopa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScopaGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
